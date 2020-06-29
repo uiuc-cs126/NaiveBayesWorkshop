d12c3a546b2470846cd6408f7b288701164c6d2c
MHokinson38 <matthew@hokinson.com>
2020-06-29 18:18:05 -0500

    init commit

---
+++ Code/interface/Calculator.h
@@ -0,0 +1,22 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+//Note: #pragma once is also suitable to use rather than the above 
+
+namespace Math {
+
+    enum Operations {
+
+    };
+
+    class Calculator {
+        public:
+            struct Equation {
+
+            };
+
+        private:
+    };
+}
+
+#endif
