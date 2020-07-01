a0898d7cd6405182be6f8a4ef5e306a97a0905eb
MHokinson38 <matthew@hokinson.com>
2020-06-30 23:17:41 -0500

    Fixed naming conventions

--- Code/interface/Calculator.h
+++ Code/interface/Calculator.h
@@ -17,44 +17,47 @@
 #define E 2.7
 */
 
-namespace Math {
-    //==================
-    // Constants (The right way)
-    //==================
-    const char SPECIAL_CHAR_DELIM = '/';
-    const std::string PI_STR = "pi";
-    const std::string E_STR = "e";
+namespace math {
+//==================
+// Constants (The right way)
+//==================
+const char kSpecialCharacterDelim = '/';
 
-    enum Operation {
-        ADD,
-        SUBTRACT,
-        MULTIPLY,
-        DIVIDE
-    };
+const std::string kPiStr = "pi";
+const std::string kEStr = "e";
+const double kPiVal = 3.14;
+const double kEVal = 2.72;
 
-    class Calculator {
-        public:
-            struct Expressions {
-                double number1;
-                double number2;
-                Operation op;
+enum Operation {
+    kAdd,
+    kSubtract,
+    kMultiply,
+    kDivide
+};
 
-                double solveEquation();
-            };
+class Calculator {
+    public:
+        struct Expression {
+            double number1_;
+            double number2_;
+            Operation op_;
 
-            //This is one way to declare a default constructor, but if not included at all, the compiler will create your class assuming this line
-            //This is only really necessary when Calculator is given multiple constructors and you want a default constructor as well 
-            Calculator() = default; 
+            double solveExpression();
+        };
 
-            void solveEquations();
+        //This is one way to declare a default constructor, but if not included at all, the compiler will create your class assuming this line
+        //This is only really necessary when Calculator is given multiple constructors and you want a default constructor as well 
+        Calculator() = default; 
 
-            //You can declare functions in line in the header file, but this should only be done with small, single line functions 
-            std::vector<std::string> getSolutions() { return solutions; }
+        void solveEquations();
 
-        private:
-            std::vector<Expressions> expressions;
-            std::vector<std::string> solutions;
-    };
-}
+        //You can declare functions in line in the header file, but this should only be done with small, single line functions 
+        inline std::vector<std::string> getSolutions() { return solutions_; }
+
+    private:
+        std::vector<Expression> expressions_;
+        std::vector<std::string> solutions_;
+};
+} //namespace math
 
 #endif
