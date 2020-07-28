7b0a8cbfa0e6a38b4396549accf2d29c258ce128
MHokinson38 <matthew@hokinson.com>
2020-07-27 22:07:34 -0500

    Fixed merge conflicts in style-refactored branch

--- src/Operation.cpp
+++ src/Operation.cpp
@@ -7,25 +7,6 @@ bool isOperationChar(char maybeOpChar) {
       maybeOpChar == kMultiplyChar || maybeOpChar == kDivideChar;
 }
 
-<<<<<<< HEAD
-=======
-Operation charToOperation(char op) {
-  switch (op) {
->>>>>>> 3130779d2a05a3356db5ed09ff65342c256c3283
-    case kAddChar:
-      return Operation::kAdd;
-    case kSubtractChar:
-      return Operation::kSubtract;
-    case kMultiplyChar:
-      return Operation::kMultiply;
-    case kDivideChar:
-      return Operation::kDivide;
-  }
-
-  return Operation::kNoOperation;
-}
-
-
 std::ostream& operator<<(std::ostream& os, const Operation& op) {
   switch (op) {
     case kAdd:
